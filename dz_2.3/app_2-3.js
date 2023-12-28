a9527bfc0a2dc0397f1c743abd6998b5ab2c6bc2
Yunadzuki <yunadzuki@mail.ru>
2023-12-28 22:26:08 +0300

    dz_2.3 task

---
+++ dz_2.3/app_2-3.js
@@ -0,0 +1,12 @@
+const titleProject = 'DZ-2.3'
+const screensValue = [
+	'шаблонные',
+	'с уникальным дизайном',
+	'с анимациями'
+]
+const screenPrice = 10000n;
+const percentage = 10;
+const fullPrice = 10000n;
+const bool = {
+	responsive: true} 
+console.log(typeof responsive)
