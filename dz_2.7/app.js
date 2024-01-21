f843b0b25d26cd7cf3969c7aa79b9aa74ee807db
Yunadzuki <yunadzuki@mail.ru>
2024-01-21 13:42:05 +0300

    dz_2.7 task

--- dz_2.7/app.js
+++ dz_2.7/app.js
@@ -1,59 +1,75 @@
-
 const gameBotFunction = function () {
 	function randomGenerate(min, max) {
-		let rand = min - 0.5 + Math.random() * (max - min + 1)
-		return Math.round(rand)
+			return Math.floor(min + Math.random() * (max - min + 1));
 	}
-	let mysteryNumber = randomGenerate(1, 100)
-	let answer = ''
-	let tries = 5;
 
-	const isNumber = function(num) {
-		return !isNaN(parseFloat(num)) && isFinite(num)
-	}
-	
-	return function getResult() {
-		answer = prompt('Угадайте число от 1 до 100')
-		if(answer === null) {
-			alert('Конец игры')
-			return
-		}
-	
-		while(!isNumber(answer) || answer.trim() === '') {
-			alert('Вы ввели строку:')
-			answer = prompt('Угадайте число от 1 до 100')
-		}
-		answer = Number(answer)
+	let mysteryNumber;
+	let tries;
 
-		if(mysteryNumber > answer) {
-				alert('Число больше: ' + 'Осталось попыток: ' + tries)
-			} else if(mysteryNumber < answer) {
-				alert('Число меньше: ' + 'Осталось попыток: ' + tries)
-			} else if(mysteryNumber === answer) {
-				const isUserWantNewGame = confirm('Поздравляем, вы угадали! Хотите сыграть снова?')
-			} if(isUserWantNewGame) {
-					tries = 2
-					mysteryNumber = randomGenerate(1, 100)
-			} else {
-					tries = 0
+	const isNumber = function (num) {
+			return !isNaN(parseFloat(num)) && isFinite(num);
+	};
+
+	const playAgain = function () {
+			return confirm('Хотите сыграть снова?');
+	};
+
+	const getResult = function () {
+			let answer = prompt(`Угадайте число от 1 до 100. Осталось попыток: ${tries}`);
+
+			if (answer === null) {
+					alert('Конец игры');
+					return;
 			}
-		}
 
-		
-		if (tries > 0) {
-				tries ==
-				getResult();
-		} else {
-			const again = confirm('Попыток больше нет, конец игры. Хотите ещё?')
-			if (again) {
-				tries = 5
-				getResult();
-			} else {
-				alert('Конец игры')
-				return
+			while (!isNumber(answer) || answer.trim() === '') {
+					alert('Вы ввели некорректное значение.');
+					answer = prompt(`Угадайте число от 1 до 100. Осталось попыток: ${tries}`);
 			}
-		}
-}
+
+			answer = Number(answer);
+
+			if (mysteryNumber > answer) {
+					alert('Число больше.');
+			} else if (mysteryNumber < answer) {
+					alert('Число меньше.');
+			} else {
+					const isUserWantNewGame = playAgain();
+
+					if (isUserWantNewGame) {
+							tries = 5;
+							mysteryNumber = randomGenerate(1, 100);
+					} else {
+							alert('Конец игры');
+							return;
+					}
+			}
+
+			tries--;
+
+			if (tries > 0) {
+					getResult();
+			} else {
+					const again = confirm('Попыток больше нет, конец игры. Хотите ещё?');
+
+					if (again) {
+							tries = 5;
+							mysteryNumber = randomGenerate(1, 100);
+							getResult();
+					} else {
+							alert('Конец игры');
+							return;
+					}
+			}
+	};
+
+	return function () {
+			mysteryNumber = randomGenerate(1, 100);
+			tries = 5;
+			getResult();
+	};
+};
+
 let launchGameBot = gameBotFunction();
 launchGameBot();
 
